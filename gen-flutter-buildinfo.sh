cfa863ea460dc4a1d3895906b3e13114da5c52fe
Advay Mengle <source@madvay.com>
2020-07-08 16:30:27 -0700

    Create gen-flutter-buildinfo.sh

---
+++ gen-flutter-buildinfo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euv
+
+git diff --exit-code
+
+GIT_SHA="$(git rev-parse --verify HEAD)"
+BUILT_AT="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+
+cat <<EOF > client/flutter/lib/generated/build.dart
+class BuildInfo {
+  static const POPULATED = true;
+  static const GIT_SHA = "$GIT_SHA";
+  static const BUILT_AT = "$BUILT_AT";
+  static const DEVELOPMENT_ONLY = $DEVELOPMENT_ONLY;
+}
+EOF
